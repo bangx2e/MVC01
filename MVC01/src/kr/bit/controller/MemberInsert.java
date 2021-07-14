de9081336e6b1b549a43b79fbc9c8b35bfaf0447
Chanwoo Lim <zzanga9244@gmail.com>
2021-07-15 05:37:24 +0900

    2021-07-15 NarP MVC parameter로 값받아서 VO에 넣기

---
+++ MVC01/src/kr/bit/controller/MemberInsert.java
@@ -0,0 +1,37 @@
+package kr.bit.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.bit.model.MemberVO;
+
+@WebServlet("/memberInsert.do")
+public class MemberInsert extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	// 1. 파라메터 수집 (VO)
+	String id = request.getParameter("id");
+	String pass = request.getParameter("pass");
+	String name = request.getParameter("name");
+	int age = Integer.parseInt(request.getParameter("age"));
+	String email = request.getParameter("email");
+	String phone = request.getParameter("phone");
+//	System.out.println(id);
+	MemberVO mem = new MemberVO(id,pass,name,age,email,phone);
+	MemberVO vo = new MemberVO();
+	vo.setId(id);
+	vo.setPass(pass);
+	vo.setName(name);
+	vo.setAge(age);
+	vo.setEmail(email);
+	vo.setPhone(phone);
+	System.out.println(mem.toString());
+	System.out.println(vo+"투스트링");
+	}
+
+}
