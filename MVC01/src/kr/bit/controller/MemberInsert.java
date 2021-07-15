ddf6ece8ba13adaea89ade8508e19b831e26b9f6
Chanwoo Lim <zzanga9244@gmail.com>
2021-07-16 03:43:28 +0900

    2021-07-16 JDBC Connect, MemberDAO

--- MVC01/src/kr/bit/controller/MemberInsert.java
+++ MVC01/src/kr/bit/controller/MemberInsert.java
@@ -12,26 +12,29 @@ import kr.bit.model.MemberVO;
 @WebServlet("/memberInsert.do")
 public class MemberInsert extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	// 1. 파라메터 수집 (VO)
-	String id = request.getParameter("id");
-	String pass = request.getParameter("pass");
-	String name = request.getParameter("name");
-	int age = Integer.parseInt(request.getParameter("age"));
-	String email = request.getParameter("email");
-	String phone = request.getParameter("phone");
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// Request에서 넘어오는 데이터의 인코딩을 UTF-8로 설정한다
+		request.setCharacterEncoding("UTF-8");
+		// 1. 파라메터 수집 (VO)
+		String id = request.getParameter("id");
+		String pass = request.getParameter("pass");
+		String name = request.getParameter("name");
+		int age = Integer.parseInt(request.getParameter("age"));
+		String email = request.getParameter("email");
+		String phone = request.getParameter("phone");
 //	System.out.println(id);
-	MemberVO mem = new MemberVO(id,pass,name,age,email,phone);
-	MemberVO vo = new MemberVO();
-	vo.setId(id);
-	vo.setPass(pass);
-	vo.setName(name);
-	vo.setAge(age);
-	vo.setEmail(email);
-	vo.setPhone(phone);
-	System.out.println(mem.toString());
-	System.out.println(vo+"투스트링");
+		MemberVO mem = new MemberVO(id, pass, name, age, email, phone);
+		MemberVO vo = new MemberVO();
+		vo.setId(id);
+		vo.setPass(pass);
+		vo.setName(name);
+		vo.setAge(age);
+		vo.setEmail(email);
+		vo.setPhone(phone);
+		System.out.println(mem.toString());
 	}
 
 }
--- MVC01/src/kr/bit/model/MemberDAO.java
+++ MVC01/src/kr/bit/model/MemberDAO.java
@@ -1,10 +1,31 @@
 package kr.bit.model;
+
 // JDBC ->myBatis, JPA
 import java.sql.*;
+
 public class MemberDAO {
 	private Connection conn;
 	private PreparedStatement ps;
 	private ResultSet rs;
-	
-	
+
+	// 데이터베이스 연결객체 생성
+	public void getConnect() {
+		String url = "jdbc:mysql://localhost:3306/test?characterEncoding=UTF-8&serverTimeZone=UTC";
+		String user = "root";
+		String password = "admin12345";
+		// MySQL 드라이버 로딩
+		try {
+			// 동적로딩(실행시점에서 객체를 생성하는 방법)
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url, user, password);
+			ps = conn.prepareStatement("select * from member");
+			rs = ps.executeQuery();
+			if (rs.next()) {
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
